7e818e834173d0f402472d850e6db990aa67f2f2
Deepan Anbarasan <cantiusdeepan@gmail.com>
2019-10-02 16:32:01 +0200

    Added image modify and manipulation code for profile picture

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,9 +1,10 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField,FileAllowed
 from flask_login import current_user
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import (DataRequired, Length, Email, EqualTo,
                                 ValidationError)
+
 from flaskblog.models import User
 
 
@@ -16,13 +17,13 @@ class RegistrationForm(FlaskForm):
         DataRequired()), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    def validate_username(self,username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken! Please choose '
                                   'another username!!')
 
-    def validate_email(self,email):
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken! Please use '
@@ -40,18 +41,18 @@ class UpdateAccountForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(
             min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    picture = FileField('Update profile Picture', validators=[FileAllowed(
-            'jpg','png')])
+    picture = FileField('Update profile Picture',
+                        validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
-    def validate_username(self,username):
+    def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError('That username is taken! Please choose '
                                       'another username!!')
 
-    def validate_email(self,email):
+    def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,12 @@
-from flask import  render_template, url_for, flash, redirect,request
-from flaskblog import app,db,bcrypt
+import os
+import secrets
+from  PIL import Image
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, current_user, logout_user, login_required
+
+from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
-from flaskblog.models import User,Post
-from flask_login import login_user,current_user,logout_user,login_required
+from flaskblog.models import User
 
 posts = [
     {
@@ -40,7 +44,7 @@ def register():
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(
                 form.password.data).decode('utf-8')
-        user = User(username=form.username.data,email = form.email.data,
+        user = User(username=form.username.data, email=form.email.data,
                     password=hashed_password)
         db.session.add(user)
         db.session.commit()
@@ -59,11 +63,12 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password,
                                                form.password.data):
-            login_user(user,remember=form.remember.data)
+            login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             flash('Logged in successfully!',
-                 'success')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+                  'success')
+            return redirect(next_page) if next_page else redirect(
+                url_for('home'))
 
         else:
             flash('Login Unsuccessful. Please check email and password',
@@ -76,20 +81,39 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+
+def save_picture(form_picture):
+    # using random hex to use a random file name for the image
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics',
+                                picture_fn)
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file_name = save_picture(form.picture.data)
+            current_user.image_file = picture_file_name
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
-        flash('Your account has been updated!','success')
+        flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
     if request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-    image_file = url_for('static',filename='profile_pics/'+current_user.image_file)
+    image_file = url_for('static',
+                         filename='profile_pics/' + current_user.image_file)
     print("image_file:", image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
