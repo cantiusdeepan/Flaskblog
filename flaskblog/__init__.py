15a537bbc9794c2c6e5a1e429dcb885ba6e44fca
Deepan Anbarasan <cantiusdeepan@gmail.com>
2019-10-28 12:29:49 +0100

    Added custom error pages using blueprint

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -20,6 +20,7 @@ def create_app(config_class=Config):
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
@@ -27,5 +28,6 @@ def create_app(config_class=Config):
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
